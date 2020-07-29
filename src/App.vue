1671e8749fc9c9c0ad060d6ced603a010f9cd5f3
Sergio Ruiz Pérez <sergiotto@outlook.com>
2020-07-29 09:37:55 +0200

    pagina de login

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,50 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link v-if="authenticated" to="/login" v-on:click.native="logout()" replace>Logout</router-link>
     </div>
-    <router-view />
+    <router-view @authenticated="setAuthenticated" />
   </div>
 </template>
 
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      authenticated: false,
+      mockAccount: {
+        username: "admin",
+        password: "test"
+      }
+    };
+  },
+  mounted() {
+    if (!this.authenticated) {
+      this.$router.replace({ name: "login" });
+    }
+  },
+  methods: {
+    setAuthenticated(status) {
+      this.authenticated = status;
+    },
+    logout() {
+      this.authenticated = false;
+    }
+  }
+};
+</script>
+
 <style>
+body {
+  background-color: #f0f0f0;
+}
+h1 {
+  padding: 0;
+  margin-top: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  width: 1024px;
+  margin: auto;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,27 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import LoginComponent from '../views/Login.vue';
+import SecureComponent from '../views/Secure.vue';
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
-];
-
-const router = new VueRouter({
-  routes
+export default new VueRouter({
+  routes: [
+    {
+      path: '/',
+      redirect: {
+        name: 'login',
+      },
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginComponent,
+    },
+    {
+      path: '/secure',
+      name: 'secure',
+      component: SecureComponent,
+    },
+  ],
 });
-
-export default router;
